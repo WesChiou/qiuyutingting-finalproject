88489cdbec71587b7b47b041b69d607975eea063
qiuyuting <qiuyuting@zjnu.edu.cn>
2024-12-22 19:02:49 +0800

    token签发和校验；相册增删改查。

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 var Koa = require('koa');
 const { koaBody } = require('koa-body');
 var Router = require('koa-router');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
+var jwt = require('jsonwebtoken');
 
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
@@ -13,33 +15,234 @@ var router = new Router();
 app.use(koaBody());
 
 router.get('/', (ctx, next) => {
-  // ctx.router available
-  ctx.body = 'hello world';
+  ctx.body = { };
 });
 
 router.post('/signup', async (ctx, next) => {
-  console.log(ctx.request);
-  console.log(ctx.request.body);
-  if (!ctx.request.body.email) {
+  const { username, password } = ctx.request.body;
+
+  if (!username) {
     ctx.status = 400;
-    ctx.body = {msg: '请输入邮箱！'};
+    ctx.body = { msg: '请输入用户名！' };
     return;
-  } else if (!ctx.request.body.password) {
+  } else if (!password) {
     ctx.status = 400;
-    ctx.body = {msg: '请输入密码！'};
+    ctx.body = { msg: '请输入密码！' };
+    return;
+  } else if (typeof username !== 'string') {
+    ctx.status = 400;
+    ctx.body = { msg: '用户名必须为字符串！' };
+    return;
+  } else if (username.length < 6 || username.length > 64) {
+    ctx.status = 400;
+    ctx.body = { msg: '用户名不能少于6个字符或大于64个字符！' };
+    return;
+  } else if (typeof password !== 'string') {
+    ctx.status = 400;
+    ctx.body = { msg: '密码必须为字符串！' };
+    return;
+  } else if (password.length < 8 || password.length > 32) {
+    ctx.status = 400;
+    ctx.body = { msg: '密码不能少于8个字符或大于32个字符！' };
+    return;
+  }
+
+  await client.connect();
+  const db = client.db(DB_NAME);
+  const collection = db.collection('user');
+  const user = await collection.findOne({ username });
+
+  if (user) {
+    ctx.status = 409;
+    ctx.body = { msg: '用户已存在！' };
+  } else {
+    await collection.insertOne({ username, password });
+    ctx.status = 201;
+    ctx.body = {msg: '创建成功！'};
+  }
+  await client.close();
+
+});
+
+router.post('/login', async (ctx, next) => {
+  const { username, password } = ctx.request.body;
+  await client.connect();
+  const db = client.db(DB_NAME);
+  const collection = db.collection('user');
+  const user = await collection.findOne({ username, password });
+
+  if (user) {
+    const token = jwt.sign({ 
+      username, 
+      password, 
+      id: user._id.toString(), 
+    }, process.env.JWT_SECRET_KEY);
+    ctx.body = { msg: '登陆成功！', token };
+  } else {
+    ctx.status = 404;
+    ctx.body = { msg: '用户名或密码错误！' };
+  }
+  await client.close();
+});
+
+router.post('/album', async (ctx, next) =>{
+  const { name } = ctx.request.body;
+
+  if (!name || typeof name !== 'string') {
+    ctx.status = 400;
+    ctx.body = { msg: '相册名不能为空或非字符串！' };
+    return;
+  } else if (name.length > 64) {
+    ctx.status = 400;
+    ctx.body = { msg: '相册名不能超过64个字符！' };
+    return;
+  }
+
+  const authorization = ctx.headers['authorization'];
+  const token = authorization.split(' ')[1];
+  var decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+  await client.connect();
+  const db = client.db(DB_NAME);
+  const collection = db.collection('album');
+  const album = await collection.findOne({ name, user_id: decoded.id });
+
+  if (album) {
+    ctx.status = 409;
+    ctx.body = { msg: `相册 ${name} 已存在！` };
+  } else {
+    await collection.insertOne({ name, user_id: decoded.id });
+    ctx.status = 201;
+    ctx.body = { msg: '创建成功！' };
+  }
+  await client.close();
+
+});
+
+router.get('/album', async (ctx, next) => {
+  const authorization = ctx.headers['authorization'];
+  if (!authorization){
+    ctx.status = 401;
+    ctx.body = { msg: '未授权！' };
+    return;
+  }
+  const token = authorization.split(' ')[1];
+  try {
+    var decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  } catch (err) {
+    ctx.status = 401;
+    ctx.body = { msg: '未授权！' };
+    return;
+  }
+
+  await client.connect();
+  const db = client.db(DB_NAME);
+  const collection = db.collection('album');
+  const albums = await collection.find({ user_id: decoded.id }).toArray();
+
+  ctx.status = 200;
+  ctx.body = { 
+    msg: '获取成功！', 
+    albums: albums.map((item) => {
+      return { 
+        name: item.name,
+        id: item._id.toString(),
+      };
+    }),
+  };
+  await client.close();
+
+});
+
+router.delete('/album/:id', async (ctx, next) => {
+  const { id } = ctx.params;
+  if (!id || typeof id !== 'string') {
+    ctx.status = 400;
+    ctx.body = { msg: '请输入相册id！' };
+    return;
+  }
+
+  const authorization = ctx.headers['authorization'];
+  if (!authorization){
+    ctx.status = 401;
+    ctx.body = { msg: '未授权！' };
+    return;
+  }
+  const token = authorization.split(' ')[1];
+  try {
+    var decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  } catch (err) {
+    ctx.status = 401;
+    ctx.body = { msg: '未授权！' };
+    return;
+  }
+
+  await client.connect();
+  const db = client.db(DB_NAME);
+  const collection = db.collection('album');
+  const result = await collection.deleteOne({ user_id: decoded.id, _id: new ObjectId(id) });
+  console.log(result);
+  if (result.deletedCount) {
+    ctx.status = 200;
+    ctx.body = { msg: '删除成功！' };
+  } else {
+    ctx.status = 404;
+    ctx.body = { msg: '相册不存在!' };
+  }
+  await client.close();
+
+});
+
+router.put('/album/:id', async (ctx, next) => {
+  const { id } = ctx.params;
+  if (!id || typeof id !== 'string') {
+    ctx.status = 400;
+    ctx.body = { msg: '请输入相册id！' };
+    return;
+  }
+
+  const { name } = ctx.request.body;
+  if (!name || typeof name !== 'string') {
+    ctx.status = 400;
+    ctx.body = { msg: '相册名不能为空或非字符串！' };
+    return;
+  } else if (name.length > 64) {
+    ctx.status = 400;
+    ctx.body = { msg: '相册名不能超过64个字符！' };
+    return;
+  }
+
+  const authorization = ctx.headers['authorization'];
+  if (!authorization){
+    ctx.status = 401;
+    ctx.body = { msg: '未授权！' };
+    return;
+  }
+  const token = authorization.split(' ')[1];
+  try {
+    var decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  } catch (err) {
+    ctx.status = 401;
+    ctx.body = { msg: '未授权！' };
     return;
   }
   await client.connect();
-  console.log('Connected successfully to server');
   const db = client.db(DB_NAME);
-  const collection = db.collection('user');
-  const insertResult = await collection.insertOne({
-    email: ctx.request.body.email, 
-    password: ctx.request.body.password,
-  });
-  console.log(insertResult);
-  ctx.status = 201;
-  ctx.body = {msg: '创建成功！'};
+  const collection = db.collection('album');
+  const result = await collection.updateOne(
+    { _id: new ObjectId(id) }, 
+    { $set: { name } } 
+  );
+  if (result.matchedCount) {
+    ctx.status = 200;
+    ctx.body = { msg: "相册更新成功!" };
+  } else {
+    ctx.status = 404;
+    ctx.body = { msg: "相册不存在!" };
+  }
+
+  await client.close();
+  
 });
 
 app
